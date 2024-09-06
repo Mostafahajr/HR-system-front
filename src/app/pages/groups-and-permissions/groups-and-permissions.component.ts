2e307059430eccd66fd6fe917e4c26668de81c5b
am33mar <ammar011246@gmail.com>
2024-09-06 21:24:32 +0300

    fix breadcrumbs navigation between child and parent in groups

--- src/app/pages/groups-and-permissions/groups-and-permissions.component.ts
+++ src/app/pages/groups-and-permissions/groups-and-permissions.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { BreadcrumbsComponent } from '../../components/breadcrumbs/breadcrumbs.component';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-groups-and-permissions',
   standalone: true,
-  imports: [BreadcrumbsComponent, MatButton],
+  imports: [BreadcrumbsComponent, MatButton, RouterOutlet,NgIf],
   templateUrl: './groups-and-permissions.component.html',
   styleUrl: './groups-and-permissions.component.scss',
 })
@@ -15,5 +16,8 @@ export class GroupsAndPermissionsComponent {
 
   navigate(route: string): void {
     this.router.navigate([route]);
+  }
+  isAddNewGroupRoute(): boolean {
+    return this.router.url === '/groups-and-permissions';
   }
 }
