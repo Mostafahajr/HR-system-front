37f9bc176c630550e3d419d67cdde6a60b7e7cc5
FedaaWael <fedaawael29@gmail.com>
2024-09-07 17:26:24 +0300

    employeesApi

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { EmployeesService } from './../../services/employees/employees.service';
+import { Component, OnInit } from '@angular/core';
 import { BreadcrumbsComponent } from '../../components/breadcrumbs/breadcrumbs.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [BreadcrumbsComponent],
+  imports: [BreadcrumbsComponent ,ReactiveFormsModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
+  // providers:[EmployeesService],
 })
-export class EmployeesComponent {}
+export class EmployeesComponent implements OnInit{
+  employees:any;
+  constructor(private EmployeesServices:EmployeesService){
+    }
+    employeeForm =new FormGroup({
+      name: new FormControl(),
+      salary: new FormControl()
+  });
+  ngOnInit():void{
+    this.employee
+}
+edithandeler(){
+  this.EmployeesServices.editEmployee(this.employee, this.employeeForm.value)
+    .subscribe({
+      next: (response) => {
+        console.log('Employee Edited Successfully',response);
+      },
+    });
+}
+updateEmployee(){
+  this.EmployeesServices.updateEmployee(this.employees.id, this.employeeForm.value).subscribe({
+    next: (response) => {
+      console.log('Employee updated successfully', response);
+      this.employee;
+}
+});
+}
+deleteHandler(employeeId: any) {
+this.EmployeesServices.deleteEmployee(employeeId).subscribe({
+  next: () => {
+    this.employees = this.employees.filter(
+      (employee: any) => employee.id != employeeId
+    );
+  },
+});
+}
+    employee(e: any) {
+      e.preventDefault();
+      this.EmployeesServices.getAllEmployees().subscribe({
+        next: (response) => {
+          this.employees=response;
+          this.EmployeesServices.addNewEmployee(this.employeeForm.value).subscribe({
+            next:(response)=>{
+              console.log('Employee Added Successfully',response);
+            }
+          })
+        },
+
+      });
+    }
+ 
+    }
+
